2b8e1f07f865283670c511259a63b35e7126281a
Farid <fdhiuxhwfbxnaxx@gmail.com>
2025-12-29 23:01:49 +0330

    clearing

--- day10/1.js
+++ day10/1.js
@@ -15,7 +15,6 @@ for await (const line of rl) {
 	const machine = machineParts
 		.splice(1, machineParts.length - 2)
 		.reduce((acc, m, index) => (m === "#" ? [...acc, index] : acc), [])
-	// const joltage = parts[parts.length - 1]
 	const buttons = parts.splice(1, parts.length - 2).map(
 		b =>
 			new Set(
@@ -25,24 +24,12 @@ for await (const line of rl) {
 					.map(d => +d)
 			)
 	)
-	// console.log(buttons)
-
-	// const buttonOptionsOf = {}
-	// for (let i = 0; i < buttons.length; i++) {
-	// 	for (const light of buttons[i]) {
-	// 		buttonOptionsOf[light] ??= []
-	// 		buttonOptionsOf[light].push(i)
-	// 	}
-	// }
-
 	let lowestPresses = Number.MAX_SAFE_INTEGER
-	let lowest = {}
 	for (let i = 0; i < 2 ** buttons.length - 1; i++) {
 		let presses = 0
 		const result = new Set()
 		let selection = i.toString(2)
 		const space = buttons.length - selection.length
-		// console.log({ space, selection })
 		for (let u = 0; u < space; u++) selection = "0" + selection
 		for (let j = 0; j < selection.length; j++) {
 			const bIndex = selection[j]
@@ -54,17 +41,12 @@ for await (const line of rl) {
 				presses++
 			}
 		}
-		// console.log({ selection, result, presses })
-		// console.log({ result, machine })
 		if (
 			result.size === machine.length &&
 			machine.every(m => result.has(m)) &&
 			presses < lowestPresses
-		) {
+		)
 			lowestPresses = presses
-			lowest.selection = selection
-			lowest.result = [...result]
-		}
 	}
 	password += lowestPresses
 }
