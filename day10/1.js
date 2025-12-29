7571a96e19588742d0cdd1015dec4b2896beaf98
Farid <fdhiuxhwfbxnaxx@gmail.com>
2025-12-29 22:58:30 +0330

    10-1

--- day10/1.js
+++ day10/1.js
@@ -1,69 +1,72 @@
 import fs from "node:fs"
 import readline from "node:readline"
 
-// const fileStream = fs.createReadStream("input.txt")
-const fileStream = fs.createReadStream("input-test.txt")
+const fileStream = fs.createReadStream("input.txt")
+// const fileStream = fs.createReadStream("input-test.txt")
 const rl = readline.createInterface({
 	input: fileStream,
 	crlfDelay: Infinity,
 })
 
-const input = []
-let sum = 0
+let password = 0
 for await (const line of rl) {
 	const parts = line.split(" ")
 	const machineParts = parts[0].split("")
-	const machine = machineParts.splice(1, machineParts.length - 2)
+	const machine = machineParts
+		.splice(1, machineParts.length - 2)
+		.reduce((acc, m, index) => (m === "#" ? [...acc, index] : acc), [])
 	// const joltage = parts[parts.length - 1]
 	const buttons = parts.splice(1, parts.length - 2).map(
 		b =>
 			new Set(
 				b
-					.slice(1, b.length - 1)
+					.slice(1, -1)
 					.split(",")
 					.map(d => +d)
 			)
 	)
+	// console.log(buttons)
 
-	const lightStacks = Array(machine.length).fill([[]])
-	const machineShapes = Array(machine.length).fill({})
-	let lowestYet = Infinity
-	let isRunning = true
-	console.log("before while")
-	while (isRunning) {
-		for (let i = 0; i < machine.length; i++) {
-			const light = machine[i]
-			if (light === ".") {
-				continue
+	// const buttonOptionsOf = {}
+	// for (let i = 0; i < buttons.length; i++) {
+	// 	for (const light of buttons[i]) {
+	// 		buttonOptionsOf[light] ??= []
+	// 		buttonOptionsOf[light].push(i)
+	// 	}
+	// }
+
+	let lowestPresses = Number.MAX_SAFE_INTEGER
+	let lowest = {}
+	for (let i = 0; i < 2 ** buttons.length - 1; i++) {
+		let presses = 0
+		const result = new Set()
+		let selection = i.toString(2)
+		const space = buttons.length - selection.length
+		// console.log({ space, selection })
+		for (let u = 0; u < space; u++) selection = "0" + selection
+		for (let j = 0; j < selection.length; j++) {
+			const bIndex = selection[j]
+			if (bIndex === "1") {
+				buttons[j].forEach(light => {
+					if (result.has(light)) result.delete(light)
+					else result.add(light)
+				})
+				presses++
 			}
-			// console.log({ i, lightStacks, machineShapes })
-			// console.log("before for stack")
-			for (let j = 0; j < lightStacks[i].length; j++) {
-				machineShapes[`${i},${j}`] ??= {}
-				machineShapes[`${i},${j}`][machine] = "eeee"
-				const level = lightStacks[i][j]
-				// console.log({ level })
-				if (level.length > lowestYet) continue
-				for (const button of buttons) {
-					let tempMachine = [...machine]
-					if (button.has(i)) {
-						level.push(button)
-						if (light === "#") tempMachine[i] = "."
-						else tempMachine[i] = "#"
-					}
-					machineShapes[`${i},${j}`][tempMachine] = "eeee"
-					// console.log({ tempMachine })
-					if (tempMachine.filter(m => m !== ".").length === 0) {
-						console.log({ level })
-						if (lowestYet > level.length) lowestYet = level.length
-						isRunning = false
-					} else if (machineShapes[`${i},${j}`][tempMachine] === "eeee") isRunning = false
-				}
-				// lightStacks[i].push(level)
-			}
-			// console.log({ lightLevels })
+		}
+		// console.log({ selection, result, presses })
+		// console.log({ result, machine })
+		if (
+			result.size === machine.length &&
+			machine.every(m => result.has(m)) &&
+			presses < lowestPresses
+		) {
+			lowestPresses = presses
+			lowest.selection = selection
+			lowest.result = [...result]
 		}
 	}
-
-	console.log({ machine, lowestYet })
+	password += lowestPresses
 }
+
+console.log({ password })
