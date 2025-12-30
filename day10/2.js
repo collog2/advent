74f221496fd2bfd6a532211a84aa9317a009e65c
Farid <fdhiuxhwfbxnaxx@gmail.com>
2025-12-31 02:10:29 +0330

    lots of optimizations

--- day10/2.js
+++ day10/2.js
@@ -1,12 +1,27 @@
 import fs from "node:fs"
 import readline from "node:readline"
 
-// const fileStream = fs.createReadStream("input.txt")
-const fileStream = fs.createReadStream("input-test.txt")
+const fileStream = fs.createReadStream("input.txt")
+// const fileStream = fs.createReadStream("input-test.txt")
 const rl = readline.createInterface({
 	input: fileStream,
 	crlfDelay: Infinity,
 })
+
+function isCorrect(selection, buttons, joltages) {
+	for (const bIndex in selection) {
+		const value = selection[bIndex]
+		const button = buttons[bIndex]
+		button.forEach(jIndex => {
+			joltages[jIndex] -= value
+		})
+	}
+	for (const j of joltages) {
+		if (j < 0) return -1
+		else if (j > 0) return 0
+	}
+	return 1
+}
 
 let password = 0
 for await (const line of rl) {
@@ -24,28 +39,36 @@ for await (const line of rl) {
 					.map(d => +d)
 			)
 	)
-	let lowestPresses = Number.MAX_SAFE_INTEGER
-	for (let i = 0; i < 2 ** buttons.length - 1; i++) {
-		let selection = i.toString(2)
-		const space = buttons.length - selection.length
-		for (let u = 0; u < space; u++) selection = "0" + selection
-		let presses = 0
-		const result = new Array(joltages.length).fill(0)
-		for (let j = 0; j < selection.length; j++) {
-			if (selection[i] === "0") continue
-			buttons[j].forEach(leverIdx => {
-				result[leverIdx] += 1
-			})
-			presses++
+	const bLength = buttons.length
+	let isFound = false
+	let presses = 0
+	let lastPresses = [new Array(bLength).fill(0)]
+	// const maxJoltage = joltages.sort((a, b) => b - a)[0]
+	while (!isFound) {
+		presses++
+		console.log({ presses })
+		const checked = new Set()
+		const nowPresses = []
+		for (let btnIdx = 0; btnIdx < bLength; btnIdx++) {
+			for (let j = 0; j < lastPresses.length; j++) {
+				const selection = [...lastPresses[j]]
+				selection[btnIdx]++
+				const selecIdx = `${selection}`
+				// console.log(selecIdx)
+				if (checked.has(selecIdx)) continue
+				const result = isCorrect(selection, buttons, [...joltages])
+				if (result === 1) {
+					isFound = true
+					break
+				} else if (result === -1) continue
+				checked.add(selecIdx)
+				nowPresses.push(selection)
+			}
 		}
-		if (
-			result.every((value, index) => joltages[index] === value) &&
-			presses < lowestPresses
-		)
-			lowestPresses = presses
+		lastPresses = [...nowPresses]
 	}
-	password += lowestPresses
-	console.log(lowestPresses)
+	password += presses
+	console.log(presses)
 }
 
 console.log({ password })
